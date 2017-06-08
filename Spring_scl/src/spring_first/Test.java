7d5847ae92d0cac3e2bf55a4a9e7961050ff230a
SCL <294748882@qq.com>
2017-06-08 17:37:00 +0800

    6.8

--- Spring_scl/src/spring_first/Test.java
+++ Spring_scl/src/spring_first/Test.java
@@ -4,25 +4,29 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
-    
+
 	private static ApplicationContext ctx;
-	private static void  print(Object object) {
+
+	private static void print(Object object) {
 		System.out.println(object);
 	}
-	
+
 	public static void test1() {
-		Users users =new Users();
+		Users users = new Users();
 		users.setName("Teacher");
 		users.setAddress("hefei");
 		users.setMemo("this is my first Spring project");
 		print(users);
 	}
-	
+
 	public static void test2() {
-		ctx= new ClassPathXmlApplicationContext("applicationcontext.xml");
-		Users users =(Users) ctx.getBean("users");
+		// 使用ClassPathXmlApplicationContext的方式实例化spring的上下文
+		ctx = new ClassPathXmlApplicationContext("applicationcontext.xml");
+		// 通过ApplicationContext的getbean()方法，根据id来获取bean实例
+		Users users = (Users) ctx.getBean("users");
 		print(users);
 	}
+
 	public static void main(String[] args) {
 		test1();
 		test2();
